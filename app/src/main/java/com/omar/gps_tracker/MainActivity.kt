d2e3785bb354208312973d164a21605ab9399c34
Omar Mohamed <98606902+OmarLkhalil@users.noreply.github.com>
2022-12-08 16:22:05 +0200

    Start point

---
+++ app/src/main/java/com/omar/gps_tracker/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.omar.gps_tracker
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
+import androidx.core.content.ContextCompat
+import kotlin.math.log
+
+class MainActivity : AppCompatActivity() {
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
